e8a00cb81de80514e8098d05688ccd6fbbea4b2f
ARFarhanaAkter <arfarhana1994@gmail.com>
2024-01-26 12:05:28 +0200

    ex1

--- hello.php
+++ hello.php
@@ -1,6 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <!--printing hello-->
+
 <?php
-echo "Hello World";
+//this is printing hello world
+/*multiple
+line comment
 
+*/
 
-
+echo "<h1>Hello World</>";
 ?>
+
+</body>
+</html>
+
