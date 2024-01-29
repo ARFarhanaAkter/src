c579af114bcb71937a9fbd4428b8cdb0a871d311
ARFarhanaAkter <arfarhana1994@gmail.com>
2024-01-29 23:14:54 +0200

    ex2

---
+++ Far_task/footer.php
@@ -0,0 +1,45 @@
+<footer>
+      <div class="container py-5" id="contact">
+        <div class="row">
+          <div class="col-12 col-lg-4">
+            <h3 class="fw-bold pb-3">Visit us</h3>
+            <p>
+              1234 SEOe Lane, Cityville, <br />
+              State, Country
+            </p>
+            <a href="mailto:info@SEOexpress.com" class="text-dark"
+              >info@SEOeexpress.com</a
+            >
+          </div>
+          <div class="col-12 col-lg-4">
+            <h3 class="fw-bold pb-3">Our Link</h3>
+            <div class="d-flex flex-column">
+              <a href="#" class="text-dark">Home</a>
+              <a href="#" class="text-dark">How it works</a>
+              <a href="#" class="text-dark">Features</a>
+              <a href="#" class="text-dark">Contact</a>
+            </div>
+          </div>
+          <div class="col-12 col-lg-4">
+            <h3 class="fw-bold pb-3">Our Link</h3>
+            <div class="d-flex">
+              <a href="#" class="text-dark">Facebook</a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="text-center copyright"> &copy; 2024 all right reserved by Complete SEO Solutions</p>
+    </footer>
+
+    <script 
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" 
+    crossorigin="anonymous"></script>
+    <script 
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" 
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" 
+    crossorigin="anonymous">
+</script>
+</body>
+</html>
---
+++ Far_task/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+    crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="style.css">
+
+
+    <title>
+      Farhana
+    </title>
+    
+        
+
+</head>
+<body>
+  <div class="masthead"
+  style="background-image: url(assets/background-image.jpg);"      
+  background-size: contain;
+      background-repeat: no-repeat;
+      
+      height: 100vh;
+   >
+    <!--navigation bar -->
+
+    <header class="hero">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand  fw-bold" href="#" data-bs-toggle="tooltip" data-bs-title="A.R.Farhana Akter Rumjhum" data-bs-placement="bottom">A.R.Farhana Akter Rumjhum</a>
+
+
+          
+          <button class="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item px-2">
+                <a class="nav-link " aria-current="page" href="ex1.php">Exercise1</a>
+              </li>
+              <li class="nav-item px-2">
+                <a class="nav-link" href="ex2.php#">Exercise2</a>
+              </li>
+
+              <li class="nav-item px-2">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item px-3">
+                <a class="nav-link cta" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+
+
+      <section class="hero-section py-5 ">
+        <div class="container py-3">
+            <div class="row g-5">
+                <div class="d-flex allign-items-center justify-content-between">
+                <div class="col-12 col-lg-6">
