65e799d92d6b01893ddd13838363066f64dd2e69
ARFarhanaAkter <arfarhana1994@gmail.com>
2024-01-29 23:11:21 +0200

    ex2

---
+++ Far_task/ex2/contact.php
@@ -0,0 +1,12 @@
+<? include 'header.php'; ?>
+
+<body>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia expedita velit nam veniam neque, minus veritatis ut harum soluta error, distinctio excepturi ab culpa accusantium animi ad aspernatur nobis deserunt?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia expedita velit nam veniam neque, minus veritatis ut harum soluta error, distinctio excepturi ab culpa accusantium animi ad aspernatur nobis deserunt?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia expedita velit nam veniam neque, minus veritatis ut harum soluta error, distinctio excepturi ab culpa accusantium animi ad aspernatur nobis deserunt?</p>
+    
+</body>
+</html>
+
+<?php include 'footer.php'; ?>
---
+++ Far_task/ex2/ex2.php
@@ -0,0 +1,35 @@
+
+<? include 'header.php'; ?>
+
+<p class=" pt-4">
+                    Welcome to my PHP assignment.
+</p>
+        
+         <button class="btn mt-5">MEMBERSHIP</button>
+            </div>
+           <div class= "col-12 col-lg-6">
+            <div class="d-flex justify-content-end">
+                <img src="assets/2562464e8f207a2d77f1ec3b927e48c4.jpg" class="hero-img" alt="" />
+            </div>
+           </div>
+        </div>
+        </div>
+        </div>
+    </section>
+    </header>
+</body>
+
+<?php include 'footer.php'; ?>
+
+
+
+    
+
+   
+
+ 
+    
+
+   
+
+ 
